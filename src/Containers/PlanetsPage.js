2d3968746ecdbec5b093f848a09832f44e954fa9
jmajurel <jmajurel@smag.tech>
2019-10-01 13:38:34 +0200

    +added buffer to project

--- src/Containers/PlanetsPage.js
+++ src/Containers/PlanetsPage.js
@@ -2,19 +2,35 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPlanets } from "../Store/Actions/planets";
 import PlanetsList from "../Components/PlanetsList";
+import Buffer from "../Components/Buffer";
 
 class PlanetsPage extends Component {
-  componentDidMount() {
-    this.props.fetchPlanets();
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    }
   }
+
+  componentDidMount() {
+    this.setState({loading: true});
+    this.props.fetchPlanets()
+    .then(() => this.setState({loading: false}));
+  }
+
   render() {
     const { planets } = this.props;
     return (
-      <div className="Planets" style={{marginTop: 80}}>
-        <PlanetsList planets={planets} />
+      <div>
+        <div className="Planets" style={{marginTop: 80}}>
+          { this.state.loading ? <Buffer /> : null }
+          <PlanetsList planets={planets} />
+        </div>
       </div>
     );
   }
+  
 }
 
 const mapStateToProps = state => {
