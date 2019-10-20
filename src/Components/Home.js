dc07c97871865af9d2aedbb74f13f9ec99626b51
jmajurel <jeanfelix.majurel@gmail.com>
2019-10-20 14:25:20 +0200

    refactored styling of landing page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,8 +9,9 @@ export default function Home() {
     <div className="Home" style={{ backgroundImage: `url(${welcomePicture})` }}>
       <div className="app-welcome">
         <h1 className="app-title">My Falcon</h1>
-        <h3>Travel through space using your web browser</h3>
-        <Link to="/planets" className="btn btn-primary">
+        <h3>Travel through space</h3>
+        <h3>Using your web browser</h3>
+        <Link to="/planets" className="app-button">
           Check it out !
         </Link>
       </div>
